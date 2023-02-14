54c7df0f7b80c1ce454978abe14bb30ec36d3083
Admir Trakic <atrakic@users.noreply.github.com>
2023-02-14 20:58:50 +0100

    fix: commit changed Chart.yaml file

--- scripts/bump_chart.sh
+++ scripts/bump_chart.sh
@@ -11,9 +11,9 @@ version=${2:-$(git describe --tags --abbrev=0 2>/dev/null ||echo "0.0.1")}
 
 gsed -i "s/^version:.*/version: $version/" ./charts/"$chart"/Chart.yaml
 git add ./charts/"$chart"/Chart.yaml
+git commit --allow-empty -m "chore: release $version"
 
 oci_repo=${3:-} #ghcr.io/user/charts
-
 if [[ -n "${oci_repo}" ]]; then
   helm package charts/"$chart"
   helm push "$chart"-"$version".tgz oci://"$oci_repo"
