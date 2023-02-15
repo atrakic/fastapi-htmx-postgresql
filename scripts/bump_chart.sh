9dfdf7720a47ece97a56892f826906a9c1609e2f
Admir Trakic <atrakic@users.noreply.github.com>
2023-02-15 18:59:44 +0100

    [ci skip]: Update

--- scripts/bump_chart.sh
+++ scripts/bump_chart.sh
@@ -13,9 +13,9 @@ gsed -i "s/^version:.*/version: $version/" ./charts/"$chart"/Chart.yaml
 git add ./charts/"$chart"/Chart.yaml
 git commit --allow-empty -m "chore(version): release $version"
 
-oci_repo=${3:-} #ghcr.io/user/charts
+oci_repo=${3:-} #oci://ghcr.io/user/charts
 if [[ -n "${oci_repo}" ]]; then
   helm package charts/"$chart"
-  helm push "$chart"-"$version".tgz oci://"$oci_repo"
+  helm push "$chart"-"$version".tgz "$oci_repo"
   rm -rf "$chart"-"$version".tgz
 fi
